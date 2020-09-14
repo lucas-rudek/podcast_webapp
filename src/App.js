398f291ff5c581e67a6a903339266a3f6a798d44
Lucas Rudek <lucas-rudek@hotmail.com>
2020-09-14 16:45:37 -0300

    added styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-import "./styles.css";
 import Container from "./components/Container.jsx";
+import styled from "styled-components";
 
 export default function App(props) {
+  const App = styled.div`
+    font-family: sans-serif;
+    text-align: center;
+    margin: 0 10% 0 10%;
+  `;
   return (
-    <div className="App">
+    <App>
       <h1>Ausculta Cast</h1>
       <Container />
-    </div>
+    </App>
   );
 }
